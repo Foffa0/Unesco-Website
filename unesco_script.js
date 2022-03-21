4ac5a31c5d1baecb39a4f6bcafe4c74c73800e9a
Willi <willi.jaeger.schule@outlook.de>
2022-03-21 16:51:00 +0100

    Kommentar

--- unesco_script.js
+++ unesco_script.js
@@ -1,11 +1,12 @@
 // hide navbar on scroll down
 var prevScrollpos = window.pageYOffset;
 window.onscroll = function() {
-  var currentScrollPos = window.pageYOffset;
-  if (prevScrollpos > currentScrollPos) {
-    document.getElementById("nav-primary").style.top = "0";
-  } else {
-    document.getElementById("nav-primary").style.top = "-88px";
-  }
-  prevScrollpos = currentScrollPos;
-} 
+    var currentScrollPos = window.pageYOffset;
+    if (prevScrollpos > currentScrollPos) {
+        document.getElementById("nav-primary").style.top = "0";
+    } else {
+        document.getElementById("nav-primary").style.top = "-88px";
+    }
+    prevScrollpos = currentScrollPos;
+    //:D
+}
