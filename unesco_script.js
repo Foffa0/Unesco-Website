79f0f227c6eb1353f899a6d360e540d911fdd016
Foffa <chrisschmuck9@gmail.com>
2022-04-19 19:38:32 +0200

    blog category btns

--- unesco_script.js
+++ unesco_script.js
@@ -1,6 +1,6 @@
 // hide navbar on scroll down
 var prevScrollpos = window.pageYOffset;
-window.onscroll = function() {
+window.onscroll = function () {
     var currentScrollPos = window.pageYOffset;
     if (prevScrollpos > currentScrollPos) {
         document.getElementById("nav-primary").style.top = "0";
@@ -8,5 +8,16 @@ window.onscroll = function() {
         document.getElementById("nav-primary").style.top = "-88px";
     }
     prevScrollpos = currentScrollPos;
-    //hello there
+};
+
+
+// blog category filter
+
+// Add active class to the current control button (highlight it)
+var btnContainer = document.getElementById("blog-categories");
+var btns = btnContainer.getElementsByClassName("btn-category");
+for (var i = 0; i < btns.length; i++) {
+  btns[i].addEventListener("click", function() {
+    this.classList.toggle("btn-category-active")
+  });
 }
